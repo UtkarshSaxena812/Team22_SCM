5a8a036f928ebcb6080f33ee2036e9f23d2d4048
UtkarshSaxena812 <utkarshrajsaxena8@gmail.com>
2024-12-02 11:56:40 +0530

    Initial Commit

---
+++ Team22_SCMTests/Team22_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team22_SCMTests.swift
+//  Team22_SCMTests
+//
+//  Created by Utkarsh Raj Saxena on 02/12/24.
+//
+
+import Testing
+@testable import Team22_SCM
+
+struct Team22_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
